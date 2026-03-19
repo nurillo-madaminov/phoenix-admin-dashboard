1bae3697fbc967688482711908533c9834376eb4
Nurillo Madaminov <122042047+nurillo-madaminov@users.noreply.github.com>
2026-03-19 22:09:24 +0500

    ui bug fixed

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -105,7 +105,9 @@ watch(
     </div>
     <div v-else class="h-screen flex flex-1">
       <SideBar />
-      <div class="flex-1 h-full flex flex-col border-l w-[70vw] bg-gray-700">
+      <div
+        class="flex-1 h-full flex flex-col border-l w-[70vw] dark:bg-gray-700"
+      >
         <div v-if="chatStore.selectedUser !== null">
           <div class="navbar bg-base-300 shadow-lg z-50 px-4">
             <div class="flex flex-col">
